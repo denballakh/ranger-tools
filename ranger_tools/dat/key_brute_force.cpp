976e1601759fafd596144cae31de6ceecdd4a549
denballakh <denis.ballakh@gmail.com>
2021-09-24 08:47:32 +0300

    `key_brute_force.cpp`: command line arguments, new output format, measuring execution time

--- ranger_tools/dat/key_brute_force.cpp
+++ ranger_tools/dat/key_brute_force.cpp
@@ -1,8 +1,9 @@
+// optimization=3
 /*
 Программа для перебора ключей шифрования датников
-Она открывает датник и пытается подобрать такой ключ, 
+Открывает датник и пытается подобрать такой ключ,
     чтобы в расшифрованных данных был заголовок ZL01
-Программа выводит несколько чисел, одно из них является
+Выводит несколько чисел, одно из них является
     настоящим ключом
 
 Ключи для разных форматов датников:
@@ -14,14 +15,15 @@ keys = {
     'SR1': 0,
 }
 */
-#define FILENAME "find_keys_reload_cache.dat"
 
-#include <iostream>
-#include <fstream>
+#include <ctime>    // clock_t, clock(), CLOCKS_PER_SEC
+#include <cstdint>
+#include <cstdio>   // printf
+#include <fstream>  // ifstream
 
 using namespace std;
 
-int rnd(int seed) {
+inline int32_t rnd(int32_t seed) {
     seed = (seed % 0x1f31d) * 0x41a7 - (seed / 0x1f31d) * 0xb14;
     if (seed < 1)
         seed += 0x7fffffff;
@@ -29,42 +31,58 @@ int rnd(int seed) {
     return seed;
 }
 
-int main() {
-    ifstream file;
-    file.open(FILENAME, ios::in | ios::binary);
+int main(int argc, char *argv[]) {
+    // for (int i = 0; i < argc; i++)
+    //     printf("argv[%d] = %s\n", i, argv[i]);
 
-    if (!file.is_open()) {
-        cout << "Error while opening file!" << endl;
-        getchar();
-        return 0;
+    if (argc != 2 && argc != 3) {
+        printf("Usage: this.exe file [signed_flag]\n");
+        return 1;
     }
 
-    unsigned char hash_0 = file.get();
-    unsigned char hash_1 = file.get();
-    unsigned char hash_2 = file.get();
-    unsigned char hash_3 = file.get();
+    ifstream file;
+    string filename = argv[1];
+    file.open(filename, ios::in | ios::binary);
 
-    unsigned char seed_0 = file.get();
-    unsigned char seed_1 = file.get();
-    unsigned char seed_2 = file.get();
-    unsigned char seed_3 = file.get();
+    if (!file.is_open()) {
+        printf("Error while opening file!\n");
+        return 1;
+    }
+
+    if (argc == 3) { // Skipping 8 bytes of sign
+        for (int i = 0; i < 8; i++)
+            file.get();
+    }
+
+    uint8_t hash_0 = file.get(); // Unused
+    uint8_t hash_1 = file.get();
+    uint8_t hash_2 = file.get();
+    uint8_t hash_3 = file.get();
+
+    uint8_t seed_0 = file.get();
+    uint8_t seed_1 = file.get();
+    uint8_t seed_2 = file.get();
+    uint8_t seed_3 = file.get();
+    int32_t _seed = int32_t(seed_0) + (int32_t(seed_1) << 8) + (int32_t(seed_2) << 16) + (int32_t(seed_3) << 24);
     
-    unsigned char zl01_0 = file.get();
-    unsigned char zl01_1 = file.get();
-    unsigned char zl01_2 = file.get();
-    unsigned char zl01_3 = file.get();
+    uint8_t zl01_0 = file.get();
+    uint8_t zl01_1 = file.get();
+    uint8_t zl01_2 = file.get();
+    uint8_t zl01_3 = file.get();
 
     file.close();
-    
-    int _seed = int(seed_0) + (int(seed_1) << 8) + (int(seed_2) << 16) + (int(seed_3) << 24);
 
-    for (int key = -2147483648; key < 2147483647; key++) {
-        int seed = _seed ^ key;
+    clock_t t_start = clock();
 
-        unsigned char zl01_0_decoded;
-        unsigned char zl01_1_decoded;
-        unsigned char zl01_2_decoded;
-        unsigned char zl01_3_decoded;
+    int32_t seed;
+
+    uint8_t zl01_0_decoded;
+    uint8_t zl01_1_decoded;
+    uint8_t zl01_2_decoded;
+    uint8_t zl01_3_decoded;
+
+    for (int32_t key = -0x80000000; key < +0x7fffffff; key++) {
+        seed = _seed ^ key;
 
         seed = rnd(seed);
         zl01_0_decoded = zl01_0 ^ ((seed & 0xff) - 1);
@@ -78,10 +96,17 @@ int main() {
         seed = rnd(seed);
         zl01_3_decoded = zl01_3 ^ ((seed & 0xff) - 1);
 
-        if (zl01_0_decoded == 'Z' && zl01_1_decoded == 'L' && zl01_2_decoded == '0' && zl01_3_decoded == '1') {
-            cout << key << endl;
-        }
+        if (zl01_0_decoded == 'Z')
+        if (zl01_1_decoded == 'L')
+        if (zl01_2_decoded == '0')
+        if (zl01_3_decoded == '1' || zl01_3_decoded == '2' || zl01_3_decoded == '3')
+            printf("%+11d %#010x %c%c%c%c\n", key, key, zl01_0_decoded, zl01_1_decoded, zl01_2_decoded, zl01_3_decoded);
     }
-    cout << "END" << endl;
-    getchar();
+
+    clock_t t_end = clock();
+
+    printf("\n");
+    printf("Time taken:  %.3lf s\n", (double)(t_end - t_start) / CLOCKS_PER_SEC);
+
+    return 0;
 }
