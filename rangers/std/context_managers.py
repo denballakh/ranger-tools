69b92fc298eef2a1d450275ffebc9d8c93b7ceaa
denballakh <denis.ballakh@gmail.com>
2021-11-05 01:06:52 +0300

    Timer was moved to .std.time

--- rangers/std/context_managers.py
+++ rangers/std/context_managers.py
@@ -3,87 +3,13 @@ from __future__ import annotations
 from typing import Any, Literal, Iterator, Type
 from types import TracebackType
 
-import time
-
 from .mixins import PrintableMixin
 
-__all__ = ['Timer', 'switch']
+__all__ = ['switch']
 
 
-class Timer(PrintableMixin):
-    __slots__ = ('start_time', 'end_time', 'time')
-    start_time: float | None
-    end_time: float | None
-    time: float | None
-
-    def __init__(self) -> None:
-        self.start_time = None
-        self.end_time = None
-        self.time = None
-
-    def __enter__(self) -> Timer:
-        self.start_time = time.process_time()
-        return self
-
-    def __exit__(self, exc_type: Type[Exception], exc_value: Type[Exception] | Exception, traceback: TracebackType) -> Literal[False]:
-        self.end_time = time.process_time()
-        assert self.start_time is not None
-        self.time = self.end_time - self.start_time
-        return False
-
-class TimeMeasure(PrintableMixin):
-    __slots__ = ('fmtstr', 'cnt', 'start_time', 'end_time', 'time')
-    fmtstr: str
-    cnt: int
-    start_time: float | None
-    end_time: float | None
-    time: float | None
-
-    def __init__(self, fmtstr: str, cnt: int = 1) -> None:
-        self.fmtstr = fmtstr
-        self.start_time = None
-        self.end_time = None
-        self.time = None
-        self.cnt = cnt
-
-    def __enter__(self) -> TimeMeasure:
-        self.start_time = time.process_time()
-        return self
-
-    def __exit__(self, exc_type: Type[Exception], exc_value: Type[Exception] | Exception, traceback: TracebackType) -> Literal[False]:
-        self.end_time = time.process_time()
-        assert self.start_time is not None
-        self.time = self.end_time - self.start_time
-        self.time /= self.cnt
-
-        unit = 0
-        time1 = self.time
-        if not time1:
-            unit = None
-        else:
-            while time1 < 1:
-                time1 *= 1000
-                unit -= 1
-            while time1 >= 1000:
-                time1 /= 1000
-                unit += 1
-
-        time_s = str(round(time1, 2))
-
-        unit_s = {
-            None: '',
-            -3: 'ns',
-            -2: 'mcs',
-            -1: 'ms',
-            0: 's',
-            1: 'ks',
-            2: 'Ms',
-            3: 'Gs',
-        }
-
-        print(self.fmtstr % (time_s + ' ' + unit_s[unit]))
-        return False
-
+from .time import Timer # FIXME
+__all__.append('Timer')
 
 class switch(PrintableMixin):
     __slots__ = ('expr', 'triggered')
@@ -109,7 +35,12 @@ class switch(PrintableMixin):
     def __enter__(self) -> switch:
         return self
 
-    def __exit__(self, exc_type: Type[Exception], exc_value: Type[Exception] | Exception, traceback: TracebackType) -> Literal[False]:
+    def __exit__(
+        self,
+        exc_type: Type[Exception],
+        exc_value: Type[Exception] | Exception,
+        traceback: TracebackType,
+    ) -> Literal[False]:
         return False
 
     def __iter__(self) -> Iterator[switch]:
