689f84c72657c72c335159961b171c60e872600c
denballakh <denis.ballakh@gmail.com>
2021-12-03 01:37:23 +0300

    sentinel

--- rangers/std/sentinel.py
+++ rangers/std/sentinel.py
@@ -1,30 +1,18 @@
 from __future__ import annotations
 
-from typing import ClassVar
-import sys
+from typing import ClassVar, Final, final
 
-__all__ = ('sentinel',)
+from enum import Enum
+
+__all__ = ('sentinel', 'SentinelType')
 
 
-class sentinel:
-    _cache: ClassVar[dict[str, sentinel]] = {}
-
-    name: str
-
-    def __new__(cls, name: str) -> sentinel:
-        if name not in cls._cache:
-            cls._cache[name] = super().__new__(cls)
-
-        return cls._cache[name]
-
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-    def __str__(self) -> str:
-        return f'<{self.name!s}>'
+@final
+class SentinelType(Enum):
+    instance = None
 
     def __repr__(self) -> str:
-        return f'{type(self).__name__!s}({self.name!s})'
+        return '<sentinel>'
 
-    def __eq__(self, other: object) -> bool:
-        return self is other
+
+sentinel: Final = SentinelType.instance
