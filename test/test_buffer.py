7be3a702e85c1d88f5c4593d3cc69797c629b430
denballakh <denis.ballakh@gmail.com>
2023-02-01 00:13:03 +0300

    fix tests

--- test/test_buffer.py
+++ test/test_buffer.py
@@ -4,7 +4,6 @@ import unittest
 import math
 
 from rangers.std.buffer import Buffer, IBuffer, OBuffer, BaseBuffer
-from rangers.std._buffer import Buffer as _Buffer, IBuffer as _IBuffer, OBuffer as _OBuffer
 
 READ_WRITE_TEST_DATA: list[tuple[str, Any, bytes]] = [
     ('byte', 0, b'\0'),
@@ -14,38 +13,38 @@ READ_WRITE_TEST_DATA: list[tuple[str, Any, bytes]] = [
     ('bool', False, b'\0'),
     ('bool', True, b'\1'),
     #
-    ('char', 0, b'\0'),
-    ('char', 1, b'\1'),
-    ('char', -1, b'\xff'),
+    ('i8', 0, b'\0'),
+    ('i8', 1, b'\1'),
+    ('i8', -1, b'\xff'),
     #
-    ('uchar', 0, b'\0'),
-    ('uchar', 1, b'\1'),
-    ('uchar', 2**8 - 1, b'\xff'),
+    ('u8', 0, b'\0'),
+    ('u8', 1, b'\1'),
+    ('u8', 2**8 - 1, b'\xff'),
     #
-    ('short', 0, b'\0\0'),
-    ('short', 1, b'\1\0'),
-    ('short', -1, b'\xff\xff'),
+    ('i16', 0, b'\0\0'),
+    ('i16', 1, b'\1\0'),
+    ('i16', -1, b'\xff\xff'),
     #
-    ('ushort', 0, b'\0\0'),
-    ('ushort', 1, b'\1\0'),
-    ('ushort', 2**16 - 1, b'\xff\xff'),
+    ('u16', 0, b'\0\0'),
+    ('u16', 1, b'\1\0'),
+    ('u16', 2**16 - 1, b'\xff\xff'),
     #
-    ('int', 0, b'\0\0\0\0'),
-    ('int', 1, b'\1\0\0\0'),
-    ('int', 2, b'\2\0\0\0'),
-    ('int', -1, b'\xff\xff\xff\xff'),
+    ('i32', 0, b'\0\0\0\0'),
+    ('i32', 1, b'\1\0\0\0'),
+    ('i32', 2, b'\2\0\0\0'),
+    ('i32', -1, b'\xff\xff\xff\xff'),
     #
-    ('uint', 0, b'\0\0\0\0'),
-    ('uint', 1, b'\1\0\0\0'),
-    ('uint', 2**32 - 1, b'\xff\xff\xff\xff'),
+    ('u32', 0, b'\0\0\0\0'),
+    ('u32', 1, b'\1\0\0\0'),
+    ('u32', 2**32 - 1, b'\xff\xff\xff\xff'),
     #
-    ('long', 0, b'\0\0\0\0\0\0\0\0'),
-    ('long', 1, b'\1\0\0\0\0\0\0\0'),
-    ('long', -1, b'\xff\xff\xff\xff\xff\xff\xff\xff'),
+    ('i64', 0, b'\0\0\0\0\0\0\0\0'),
+    ('i64', 1, b'\1\0\0\0\0\0\0\0'),
+    ('i64', -1, b'\xff\xff\xff\xff\xff\xff\xff\xff'),
     #
-    ('ulong', 0, b'\0\0\0\0\0\0\0\0'),
-    ('ulong', 1, b'\1\0\0\0\0\0\0\0'),
-    ('ulong', 2**64 - 1, b'\xff\xff\xff\xff\xff\xff\xff\xff'),
+    ('u64', 0, b'\0\0\0\0\0\0\0\0'),
+    ('u64', 1, b'\1\0\0\0\0\0\0\0'),
+    ('u64', 2**64 - 1, b'\xff\xff\xff\xff\xff\xff\xff\xff'),
     #
     ('float', 0.0, b'\0\0\0\0'),
     ('float', 1.0, b'\0\0\x80?'),
@@ -91,6 +90,7 @@ READ_WRITE_TEST_DATA: list[tuple[str, Any, bytes]] = [
 # .write, .write_format, .write_struct
 # .read_str(length=...), .read_wstr(length=...)
 # .write_str(length=...), .write_wstr(length=...)
+
 
 class TestBuffer(unittest.TestCase):
     def test_construction(self) -> None:
@@ -151,9 +151,6 @@ class TestBuffer(unittest.TestCase):
         clss: Iterable[type[IBuffer]] = [
             IBuffer,
             Buffer,
-            # FastBuffer,  # type: ignore[list-item]
-            _IBuffer,
-            _Buffer,
         ]
         for cls in clss:
             for method_name, value, data in READ_WRITE_TEST_DATA:
@@ -187,7 +184,7 @@ class TestBuffer(unittest.TestCase):
                     self._assertEqualEx(value, actual_value)
                     self.assertEqual(buf.pos, len(data) + 2 * n)
 
-                # if method_name not in {'str', 'wstr'} and cls not in {FastBuffer}:  # type: ignore[comparison-overlap]
+                # if method_name not in {'str', 'wstr'}:
                 #     with self.subTest(
                 #         msg='test_3',
                 #         cls=cls,
@@ -202,14 +199,10 @@ class TestBuffer(unittest.TestCase):
                 #         self._assertEqualEx(value, actual_value)
                 #         self.assertEqual(buf.pos, 0)
 
-
     def test_write(self) -> None:
         clss: Iterable[type[OBuffer]] = [
             OBuffer,
             Buffer,
-            # FastBuffer,  # type: ignore[list-item]
-            _OBuffer,
-            _Buffer,
         ]
         for cls in clss:
             for method_name, value, data in READ_WRITE_TEST_DATA:
@@ -223,8 +216,7 @@ class TestBuffer(unittest.TestCase):
                     buf = cls()
                     meth = getattr(buf, f'write_{method_name}')
                     actual_value = meth(value)
-                    if cls not in {'FastBuffer'}:  # type: ignore[comparison-overlap]
-                        self.assertEqual(buf.pos, len(data))
+                    self.assertEqual(buf.pos, len(data))
                     self.assertEqual(bytes(buf), data)
 
                 # with self.subTest(
@@ -234,7 +226,7 @@ class TestBuffer(unittest.TestCase):
                 #     value=value,
                 #     expected_data=data,
                 # ):
-                #     if method_name not in {'str', 'wstr'} and cls not in {FastBuffer}:  # type: ignore[comparison-overlap]
+                #     if method_name not in {'str', 'wstr'}:
                 #         n = 10
                 #         buf = cls(n*b'\0')
                 #         buf.pos = n // 2
@@ -245,11 +237,237 @@ class TestBuffer(unittest.TestCase):
                 #         # print(type(buf))
 
 
+def test_speed() -> None:
+    import gc
+    from rangers.std.time import AdaptiveTimeMeasurer
+
+    gc.enable()
+    gc.collect()
+    gc.disable()
+
+    with AdaptiveTimeMeasurer(
+        target_time=0.1,
+        config_file='_buffer_bench.json',
+    ) as atm:
+        print()
+        print()
+
+        T = atm('empty loop', extra=2)
+        T.calibrate(0)
+        with T as _:
+            for _ in _:
+                pass
+        assert T.time is not None
+        T.calibrate(T.time)
+
+        with atm('calibrated empty loop') as _:
+            for _ in _:
+                pass
+
+        ba = bytearray(b'0123' * 10**8)
+        bahw1 = bytearray(b'hello world\0' * 10**8)
+        bahw2 = bytearray(b'hello world\0\0' * 10**8)
+        bazeros = bytearray(b'\0' * 10**8)
+        new_buf = lambda: cls(ba)
+
+        gc.collect()
+
+        cls: type[Buffer]
+        for cls in (Buffer,):
+            clsname = cls.__name__
+            print()
+
+            with atm(f'{clsname}()') as _:
+                for _ in _:
+                    cls()
+
+            buf = cls()
+            with atm(f'{clsname}.write(b"")') as _:
+                for _ in _:
+                    buf.write(b"")
+
+            buf = cls()
+            with atm(f'{clsname}.write([])') as _:
+                for _ in _:
+                    buf.write([])
+
+            buf = cls()
+            with atm(f'{clsname}.write_byte(7)') as _:
+                for _ in _:
+                    buf.write_byte(7)
+
+            buf = cls()
+            with atm(f'{clsname}.write_bool(True)') as _:
+                for _ in _:
+                    buf.write_bool(True)
+
+            buf = cls()
+            with atm(f'{clsname}.write_i8(-123)') as _:
+                for _ in _:
+                    buf.write_i8(-123)
+
+            buf = cls()
+            with atm(f'{clsname}.write_u8(123)') as _:
+                for _ in _:
+                    buf.write_u8(123)
+
+            buf = cls()
+            with atm(f'{clsname}.write_i16(-1234)') as _:
+                for _ in _:
+                    buf.write_i16(-1234)
+
+            buf = cls()
+            with atm(f'{clsname}.write_u16(1234)') as _:
+                for _ in _:
+                    buf.write_u16(1234)
+
+            buf = cls()
+            with atm(f'{clsname}.write_i32(-123456789)') as _:
+                for _ in _:
+                    buf.write_i32(-123456789)
+
+            buf = cls()
+            with atm(f'{clsname}.write_u32(123456789)') as _:
+                for _ in _:
+                    buf.write_u32(123456789)
+
+            buf = cls()
+            with atm(f'{clsname}.write_i64(-123456789)') as _:
+                for _ in _:
+                    buf.write_i64(-123456789)
+
+            buf = cls()
+            with atm(f'{clsname}.write_u64(123456789)') as _:
+                for _ in _:
+                    buf.write_u64(123456789)
+
+            buf = cls()
+            with atm(f'{clsname}.write_f32(1.23)') as _:
+                for _ in _:
+                    buf.write_f32(1.23)
+
+            buf = cls()
+            with atm(f'{clsname}.write_f64(1.23)') as _:
+                for _ in _:
+                    buf.write_f64(1.23)
+
+            buf = cls()
+            with atm(f'{clsname}.write_str("hello world")') as _:
+                for _ in _:
+                    buf.write_str("hello world")
+
+            buf = cls()
+            with atm(f'{clsname}.write_wstr("hello world")') as _:
+                for _ in _:
+                    buf.write_wstr("hello world")
+
+            buf = cls()
+            with atm(f'{clsname}.write_str("")') as _:
+                for _ in _:
+                    buf.write_str("")
+
+            buf = cls()
+            with atm(f'{clsname}.write_wstr("")') as _:
+                for _ in _:
+                    buf.write_wstr("")
+
+            buf = new_buf()
+            with atm(f'{clsname}.read(0)') as _:
+                for _ in _:
+                    buf.read(0)
+
+            buf = new_buf()
+            with atm(f'{clsname}.read(3)') as _:
+                for _ in _:
+                    buf.read(3)
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_byte()') as _:
+                for _ in _:
+                    buf.read_byte()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_bool()') as _:
+                for _ in _:
+                    buf.read_bool()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_i8()') as _:
+                for _ in _:
+                    buf.read_i8()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_u8()') as _:
+                for _ in _:
+                    buf.read_u8()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_i16()') as _:
+                for _ in _:
+                    buf.read_i16()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_u16()') as _:
+                for _ in _:
+                    buf.read_u16()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_i32()') as _:
+                for _ in _:
+                    buf.read_i32()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_u32()') as _:
+                for _ in _:
+                    buf.read_u32()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_i64()') as _:
+                for _ in _:
+                    buf.read_i64()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_u64()') as _:
+                for _ in _:
+                    buf.read_u64()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_f32()') as _:
+                for _ in _:
+                    buf.read_f32()
+
+            buf = new_buf()
+            with atm(f'{clsname}.read_f64()') as _:
+                for _ in _:
+                    buf.read_f64()
+
+            buf = cls(bahw1)
+            with atm(f'{clsname}.read_str()') as _:
+                for _ in _:
+                    buf.read_str()
+
+            buf = cls(bahw2)
+            with atm(f'{clsname}.read_wstr()') as _:
+                for _ in _:
+                    buf.read_wstr()
+
+            buf = cls(bazeros)
+            with atm(f'{clsname}.read_str() # empty') as _:
+                for _ in _:
+                    buf.read_str()
+
+            buf = cls(bazeros)
+            with atm(f'{clsname}.read_wstr() # empty') as _:
+                for _ in _:
+                    buf.read_wstr()
+
+
 if __name__ == '__main__':
     try:
         unittest.main()
     except SystemExit as exc:
         if exc.code == 0:
             import os
+
             # input()
             os._exit(0)
